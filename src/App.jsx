d56c1fe9ce36e4a69d9b60278a24a5bd0ed82bc7
seenurjpt <seenurjpt1402@gmail.com>
2023-08-26 23:42:37 +0530

    push code

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import "./App.css";
+import cars from "./cars.json";
+
+const App = () => {
+  const [readMore, setReadMore] = useState(true);
+
+  const handleRead = () => {
+    if (readMore === true) {
+      setReadMore(false);
+    }
+    if (readMore === false) {
+      setReadMore(true);
+    }
+  };
+  return (
+    <div className="container">
+      <h1>[ List ]</h1>
+      <div
+        className={readMore === false ? "list_container22" : "list_container"} >
+        <ul>
+          {cars.cars.map((car) => (
+            <li>{car.name}</li>
+          ))}
+        </ul>
+        <p class={readMore === false ? "read-more22" : "read-more"}>
+          <button
+            onClick={() => {handleRead()}}>
+            Read More... ▽
+          </button>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default App;
